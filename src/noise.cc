bb2b5c9264956519d54bda868c43b6cd7b80f019
Zehran Reshi <>
2025-02-20 15:55:52 -0600

    Added notes for what the function in the noise.cc was doing. It was a simple complex number rotation.

--- src/noise.cc
+++ src/noise.cc
@@ -4,14 +4,18 @@ std::complex<float> srsran_vec_gen_sine_simd(std::complex<float> amplitude,
                                              float freq, std::complex<float> *z,
                                              int len) {
   const float TWOPI = 2.0f * (float)M_PI;
-  std::complex<float> osc = std::polar(1.0f, TWOPI * freq);
+  std::complex<float> osc = std::polar(1.0f, TWOPI * freq); //Angular Velocity Calculation (W)(2(pi)(f))
   std::complex<float> phase = 1.0f;
   int i = 0;
 
   for (; i < len; i++) {
-    z[i] = amplitude * phase;
+    z[i] = amplitude * phase; //Stores the evolution of the wave. This is giving the rotation instances around the circumfence of a circle with radius = amplitude; TLDR, not a sin wave.
 
-    phase *= osc;
-  }
+    phase *= osc; // phase = phase * angular velocity This is just generating a simple rotation of a complex number in polar form
+  } //TLDR; Not a Sin wave generator
   return phase;
 }
+// s(t) = Amplitude * sin(wt + phase)
+
+//z1 = r1e^(i(theta)) Complex number representation
+//z = r(cos(angle)+(i)sin(angle)) = r (e^(i(angle))) Another Representation of a compelx numeber
